a4cdf643a197b306b25a65edc7e9bfd7bd1eaf2d
LoganMD <logan@logandev.org>
2026-04-24 00:51:02 -0700

    update

---
+++ build/CMakeFiles/Timber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Timber.dir/link.d"
+  "CMakeFiles/Timber.dir/code/Timber.cpp.o"
+  "CMakeFiles/Timber.dir/code/Timber.cpp.o.d"
+  "bin/Timber"
+  "bin/Timber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Timber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Timber.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Timber.
+# This may be replaced when dependencies are built.
